a6f30c1cb5859ba9a9eb32018a1ed29450dd8ef6
yeziyue <huasifei@huasifei.com>
2026-09-11 16:45:39 +0800

    mac80211.sh:wifi默认打开，ssid形式修改为WIFI-2G-mac后6位/WIFI-5G-mac后6位

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -174,7 +174,7 @@ detect_mac80211() {
 			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
-			set wireless.radio${devidx}.disabled=1
+			set wireless.radio${devidx}.disabled=0
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
@@ -183,8 +183,13 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.ssid=OpenWrt
 			set wireless.default_radio${devidx}.encryption=none
 EOF
+		
 		uci -q commit wireless
 
 		devidx=$(($devidx + 1))
 	done
+	mac_addr=`getmac | awk -F: '{gsub(/:/,""); print substr($0, length($0)-5)}'`
+	uci set wireless.default_radio0.ssid="WIFI-2G-${mac_addr}"
+	uci set wireless.default_radio1.ssid="WIFI-5G-${mac_addr}"
+	uci commit wireless
 }
